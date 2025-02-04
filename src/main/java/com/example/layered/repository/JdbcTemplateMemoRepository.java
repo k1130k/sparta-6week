355d91a7092e572cdc470ed0de0a6cb630ca414a
k1130k <whdgns1130@naver.com>
2025-02-04 10:46:20 +0900

    연결안돼

--- src/main/java/com/example/layered/repository/JdbcTemplateMemoRepository.java
+++ src/main/java/com/example/layered/repository/JdbcTemplateMemoRepository.java
@@ -3,11 +3,14 @@ package com.example.layered.repository;
 import com.example.layered.dto.MemoResponseDto;
 import com.example.layered.entrriry.Memo;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class JdbcTemplateMemoRepository implements  MemoRepository{
@@ -20,9 +23,19 @@ public class JdbcTemplateMemoRepository implements  MemoRepository{
 
     @Override
     public MemoResponseDto saveMemo(Memo memo) {
+        // INSERT Query 직접 작성하지 않아도 된다.
 
         SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
-        return null;
+        jdbcInsert.withTableName("memo").usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("title", memo.getTitle());
+        parameters.put("contents", memo.getContents());
+
+        // 저장 후 생성된 key값 Number 타입으로 반환하는 메서드
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+
+        return new MemoResponseDto(key.longValue(), memo.getTitle(), memo.getContents());
     }
 
     @Override
